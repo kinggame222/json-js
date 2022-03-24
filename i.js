3ccf5b56235e570b180b75d2f04fbf8c555e7301
kinggame222 <48651883+kinggame222@users.noreply.github.com>
2022-03-24 14:25:52 -0400

    examen rendu mardi

---
+++ i.js
@@ -0,0 +1,29 @@
+const v = {
+
+    "id": {
+        "nom": [
+            "william",
+            "antoine",
+            "maxence",
+            "william",
+            "antoine",
+            "maxence"
+        ],
+        "age": [
+            18,
+            "inconnu",
+            "inconu",
+            18,
+            "inconnu",
+            "inconu"
+        ]
+    }
+
+}
+const mu =
+
+function what() {
+
+
+    console.log(v.id.nom[v.id.nom.length - 1]);
+}
